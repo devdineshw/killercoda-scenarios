bca8a25c0bfe69bd92f805cbfe9e32ca8baecc6b
Dinesh Weerakkody <dev.dineshw@gmail.com>
2024-02-13 15:35:40 +0800

    adding background process

--- WSO2-APIM-4.2.0/APIM-4.2.0-Fundamentals_Getting-Started/intro/background.sh
+++ WSO2-APIM-4.2.0/APIM-4.2.0-Fundamentals_Getting-Started/intro/background.sh
@@ -1,2 +1,7 @@
-sed 's/<WSO2_WEB_HOST>/80/g' /root/apim-nginx.conf
+sed 's/PORT/80/g' /etc/killercoda/host > /tmp/uihost
+sed -i 's|https://||g' /tmp/uihost
+sed 's/PORT/8080/g' /etc/killercoda/host > /tmp/apihost
+sed -i 's|https://||g' /tmp/apihost
+sed -ie "s|<WSO2_WEB_HOST>|$(sed 's:http:http:g' /tmp/uihost)|g" /root/apim-nginx.conf
+sed -ie "s|<WSO2_API_HOST>|$(sed 's:http:http:g' /tmp/apihost)|g" /root/apim-nginx.conf
 echo done > /tmp/background0
