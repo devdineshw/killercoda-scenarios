b7617802733ef238a89c4fe5842a4a7bcde88d00
Dinesh Weerakkody <dev.dineshw@gmail.com>
2024-02-13 17:43:42 +0800

    adding background process

--- WSO2-APIM-4.2.0/APIM-4.2.0-Fundamentals_Getting-Started/intro/background.sh
+++ WSO2-APIM-4.2.0/APIM-4.2.0-Fundamentals_Getting-Started/intro/background.sh
@@ -9,6 +9,7 @@ sed -ie "s|<WSO2_WEB_HOST>|$(sed 's:http:http:g' /tmp/uihost)|g" /root/apim-ngin
 sed -ie "s|<WSO2_API_HOST>|$(sed 's:http:http:g' /tmp/apihost)|g" /root/apim-nginx.conf
 
 sed -ie "s|<HOST_NAME>|$(sed 's:http:http:g' /tmp/uihost)|g" /root/original-deployment.toml
+sed -ie "s|<GW_HOST>|$(sed 's:http:http:g' /tmp/apihost)|g" /root/original-deployment.toml
 
 openssl genrsa -passout pass:nginx -des3 -out nginx.key 2048
 host=$(cat /tmp/uihost)
