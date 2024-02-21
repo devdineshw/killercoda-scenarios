4c819d4f92244d9ad31908b866b4d219c730d119
Dinesh Weerakkody <dev.dineshw@gmail.com>
2024-02-21 12:01:17 +0800

    adding APIM 4.2.0 advanced lab 5

--- WSO2-APIM-4.2.0/APIM-4.2.0-Advanced-Lab05_Fine-Grained-Policy-XACML/intro/background.sh
+++ WSO2-APIM-4.2.0/APIM-4.2.0-Advanced-Lab05_Fine-Grained-Policy-XACML/intro/background.sh
@@ -8,11 +8,16 @@ sed 's/PORT/80/g' /etc/killercoda/host > /tmp/uihost
 sed -i 's|https://||g' /tmp/uihost
 sed 's/PORT/8080/g' /etc/killercoda/host > /tmp/apihost
 sed -i 's|https://||g' /tmp/apihost
+sed 's/PORT/81/g' /etc/killercoda/host > /tmp/ishost
+sed -i 's|https://||g' /tmp/ishost
+
 sed -ie "s|<WSO2_WEB_HOST>|$(sed 's:http:http:g' /tmp/uihost)|g" /root/apim-nginx.conf
 sed -ie "s|<WSO2_API_HOST>|$(sed 's:http:http:g' /tmp/apihost)|g" /root/apim-nginx.conf
+sed -ie "s|<WSO2_IS_HOST>|$(sed 's:http:http:g' /tmp/ishost)|g" /root/apim-nginx.conf
 
 sed -ie "s|<HOST_NAME>|$(sed 's:http:http:g' /tmp/uihost)|g" /root/original-deployment.toml
 sed -ie "s|<GW_HOST>|$(sed 's:http:http:g' /tmp/apihost)|g" /root/original-deployment.toml
+sed -ie "s|<IS_HOST>|$(sed 's:http:http:g' /tmp/ishost)|g" /root/original-is-deployment.toml
 
 openssl genrsa -passout pass:nginx -des3 -out nginx.key 2048
 host=$(cat /tmp/uihost)
@@ -29,10 +34,19 @@ cp /root/apim-nginx.conf /etc/nginx/conf.d/
 cp original-nginx.conf /etc/nginx/nginx.conf
 
 wget -O /root/wso2am-4.2.0.zip https://github.com/devdineshw/resources/raw/main/binaries/WSO2/APIM/4.2.0/wso2am-4.2.0.zip?download=true
+wget -O wso2is-6.1.0.zip https://github.com/devdineshw/resources/raw/main/binaries/WSO2/IS/6.1.0/wso2is-6.1.0.zip?download=true
 mkdir /root/apim1
+mkdir /root/is1
 unzip /root/wso2am-4.2.0.zip -d /root/apim1/
+unzip /root/wso2is-6.1.0.zip -d /root/is1/
 cp /root/original-deployment.toml /root/apim1/wso2am-4.2.0/repository/conf/deployment.toml
+cp /root/original-is-deployment.toml /root/apim1/wso2am-4.2.0/repository/conf/deployment.toml
 
 /etc/init.d/nginx restart
+
+export DEBIAN_FRONTEND=noninteractive
+cat /root/debconf-slapd.conf | debconf-set-selections
+apt install ldap-utils slapd -y
+
 
 echo done > /tmp/background0
--- WSO2-APIM-4.2.0/APIM-4.2.0-Advanced-Lab05_Fine-Grained-Policy-XACML/step1/step1.md
+++ WSO2-APIM-4.2.0/APIM-4.2.0-Advanced-Lab05_Fine-Grained-Policy-XACML/step1/step1.md
@@ -2,6 +2,10 @@
 
     `sh apim1/wso2am-4.2.0/bin/api-manager.sh start`{{exec}}
 
+    Check the logs and wait till server starts.
+
+    `tail -f apim1/wso2am-4.2.0/repository/logs/wso2carbon.log`{{exec}}
+
 - Go to the carbon management console
     {{TRAFFIC_HOST1_80}}/carbon
 
