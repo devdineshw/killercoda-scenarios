8e504e2b6de63bca529726069a71e7bb8bc02a04
Dinesh Weerakkody <dev.dineshw@gmail.com>
2024-02-28 22:03:07 +0800

    remove APIM 3.2.0 scenarios

--- WSO2-APIM-3.2.0/WSO2-Ballerina-HelloWorld/install-vsc-plugin.sh
+++
@@ -1,4 +0,0 @@
-echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
-cd /tmp && apt-get update -y && apt install libarchive-tools -y # install bsdtar
-curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/WSO2/vsextensions/ballerina/1.2.0-SNAPSHOT/vspackage | bsdtar -xvf - extension
-mv extension /opt/.katacodacode/extensions/WSO2.ballerina-1.2.0-SNAPSHOT
--- WSO2-APIM-3.2.0/WSO2-Ballerina-HelloWorld/step4.md
+++
@@ -1,4 +0,0 @@
-1) Access the web app using the following URL. Check the browser redirects using the developer console within the browser to understand how the Authorization Code grant requests.
-
-https://[[HOST_SUBDOMAIN]]-8080-[[KATACODA_HOST]].environments.katacoda.com/pickup-dispatch
-
